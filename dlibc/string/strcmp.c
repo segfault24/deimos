f8bbd1f964b75b736c88e4e9e81f23222566b021
Austin Selick-Bottos <segfault24@users.noreply.github.com>
2014-10-31 00:05:45 -0400

    implemented string.h comparison functions

--- dlibc/string/strcmp.c
+++ dlibc/string/strcmp.c
@@ -2,5 +2,21 @@
 
 int strcmp(const char *s1, const char *s2)
 {
-	return NULL;
+	char *s1p, *s2p;
+
+	s1p = (char *)s1;
+	s2p = (char *)s2;
+	while(*s1p != '\0' && *s2p != '\0' && *s1p == *s2p)
+	{
+		s1p++;
+		s2p++;
+	}
+	if(*s1p == *s2p)
+	{
+		return 0;
+	} else if(*s1p > *s2p) {
+		return 1;
+	} else {
+		return -1;
+	}
 }
