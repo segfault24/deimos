800a8efe516e659b798666b7deaba1e98ffae0f9
Austin Selick-Bottos <segfault24@users.noreply.github.com>
2014-10-30 23:17:09 -0400

    implemented more string.h functions

---
+++ dlibc/string/memset.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+void *memset(void *s, int c, size_t n)
+{
+	size_t i;
+	unsigned char *sp;
+
+	sp = (unsigned char *)s;
+	for(i=0; i<n; i++)
+	{
+		sp[i] = (unsigned char)c;
+	}
+	return s;
+}
