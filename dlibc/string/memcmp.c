45a866ca738dbbf40b01a8b5d5f6c44792f19ef0
Austin Selick-Bottos <segfault24@github.com>
2014-10-31 00:05:45 -0400

    implemented string.h comparison functions

--- dlibc/string/memcmp.c
+++ dlibc/string/memcmp.c
@@ -2,5 +2,21 @@
 
 int memcmp(const void *s1, const void *s2, size_t n)
 {
-	return NULL;
+	size_t i;
+	unsigned char *s1p, *s2p;
+
+	s1p = (unsigned char *)s1;
+	s2p = (unsigned char *)s2;
+	for(i=0; i<n; i++)
+	{
+		if(s1p[i] == s2p[i])
+		{
+			continue;
+		} else if (s1p[i] > s2p[i]) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+	return 0;
 }
--- dlibc/string/strncmp.c
+++ dlibc/string/strncmp.c
@@ -2,5 +2,24 @@
 
 int strncmp(const char *s1, const char *s2, size_t n)
 {
-	return NULL;
+	size_t i;
+	char *s1p, *s2p;
+
+	i = 0;
+	s1p = (char *)s1;
+	s2p = (char *)s2;
+	while(*s1p != '\0' && *s2p != '\0' && *s1p == *s2p && i<n-1)
+	{
+		s1p++;
+		s2p++;
+		i++;
+	}
+	if(*s1p == *s2p)
+	{
+		return 0;
+	} else if(*s1p > *s2p) {
+		return 1;
+	} else {
+		return -1;
+	}
 }
