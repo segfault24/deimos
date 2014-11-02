c7726e116eed6f72e6941cd4b5fc53595471416d
austin <austin@helios>
2014-11-02 16:42:14 -0500

    tested and debugged tty functions

--- arch/i386/tty.c
+++ arch/i386/tty.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 #include <i386/ioasm.h>
 #include <kernel/tty.h>
 
@@ -27,20 +28,17 @@ void term_putchar_t(char c)
 {
     if(c != '\n')
     {
-        term_buf[term_row*VGA_HEIGHT+term_col] = term_color | c;
+        term_buf[term_row*VGA_WIDTH+term_col] = term_color<<8 | c;
         term_col++;
-    } else {
-        term_row++;
-        term_col = 0;
     }
-    if(term_col >= VGA_WIDTH)
+    if(term_col >= VGA_WIDTH || c == '\n')
     {
         term_col = 0;
         term_row++;
         if(term_row >= VGA_HEIGHT)
         {
-            term_row = VGA_HEIGHT-1;
             term_scroll();
+            term_row = VGA_HEIGHT-1;
         }
     }
 }
@@ -49,7 +47,7 @@ void term_putchar_t(char c)
 
 void term_setcolor(uint8_t bg, uint8_t fg)
 {
-    term_color = (fg | bg<<4) << 8;
+    term_color = fg | bg<<4;
 }
 
 void term_putchar(char c)
@@ -69,21 +67,38 @@ void term_puts(const char* str)
 	term_updatecursor();
 }
 
+void term_puti(uint32_t i)
+{
+	size_t j;
+
+	term_puts("0x");
+	for(j=8; j>0; j--)
+	{
+		uint8_t hc = (i>>((j-1)*4)) & 0x0F;
+		if(hc < 0x0A)
+		{
+			term_putchar((char)(hc+0x30)); // 0-9
+		} else {
+			term_putchar((char)(hc+0x37)); // A-F
+		}
+	}
+}
+
 void term_scroll()
 {
-    size_t i,j,tmp;
+    size_t r, c, tmp;
 
-    for(i=1; i<VGA_HEIGHT; i++)
+    for(r=1; r<VGA_HEIGHT; r++)
     {
-        for(j=0; j<VGA_WIDTH; j++)
+        for(c=0; c<VGA_WIDTH; c++)
         {
-            term_buf[(i-1)*VGA_HEIGHT+j] = term_buf[i*VGA_HEIGHT+j];
+            term_buf[(r-1)*VGA_WIDTH+c] = term_buf[r*VGA_WIDTH+c];
         }
     }
-    tmp = (VGA_HEIGHT-1)*VGA_HEIGHT;
-    for(j=0; j<VGA_WIDTH; j++)
+    tmp = (VGA_HEIGHT-1)*VGA_WIDTH;
+    for(c=0; c<VGA_WIDTH; c++)
     {
-        term_buf[tmp+j] = ' ';
+        term_buf[tmp+c] = ' ';
     }
 }
 
@@ -91,8 +106,12 @@ void term_clear()
 {
 	size_t i;
 
+	term_row = 0;
+	term_col = 0;
+	// clear the buffer directly (faster than putchar)
 	for(i=0; i<VGA_HEIGHT*VGA_WIDTH; i++)
-		term_putchar_t(' ');
+		term_buf[i] = term_color<<8 | ' ';
+	term_updatecursor();
 }
 
 void term_init()
@@ -100,6 +119,4 @@ void term_init()
     term_buf = (uint16_t*)VGA_BASE_ADDR;
     term_setcolor(VGA_BLACK, VGA_GREEN);
 	term_clear();
-    term_row = 0;
-    term_col = 0;
 }
