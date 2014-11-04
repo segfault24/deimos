cecb9577758f1daa17d677e1644375ef96e42557
Austin Selick-Bottos <segfault24@users.noreply.github.com>
2014-11-04 03:25:50 -0500

    added PIT initialization (needs testing)

--- arch/i386/include/i386/pit.h
+++ arch/i386/include/i386/pit.h
@@ -5,9 +5,17 @@
 #include <stdint.h>
 
 #define PIT_CH0_DATA 0x40
-#define PIT_CH1_DATA 0x41
 #define PIT_CH2_DATA 0x42
 #define PIT_CMD 0x43
+
+// set channel 0 to mode 3 (square wave)
+#define PIT_CH0_CFG 0x36
+// with a reload value of 0xFFFF
+// which is a rate of 18.2065Hz
+#define PIT_CH0_RLDH 0xFF
+#define PIT_CH0_RLDL 0xFF
+
+//void pit_beep();
 
 void pit_init();
 
--- arch/i386/pit.c
+++ arch/i386/pit.c
@@ -1,7 +1,13 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <i386/ioasm.h>
 #include <i386/pit.h>
 
 void pit_init()
 {
+	// send our configuration
+	outb(PIT_CMD, PIT_CH0_CFG);
+	// set the reload value
+	outb(PIT_CH0_DATA, PIT_CH0_RLDL);
+	outb(PIT_CH0_DATA, PIT_CH0_RLDH);
 }
