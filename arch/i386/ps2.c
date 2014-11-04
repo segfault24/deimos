db22ff144a528c9512640e2f921fb62e9f26517e
Austin Selick-Bottos <segfault24@github.com>
2014-11-04 03:29:07 -0500

    very primitive keyboard driver

--- arch/i386/ps2.c
+++ arch/i386/ps2.c
@@ -1,10 +1,40 @@
-#include <i386/stddef.h>
-#include <i386/stdint.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <i386/isr.h>
 #include <i386/ioasm.h>
 #include <i386/ps2.h>
 
+#include <kernel/tty.h>
+
+static char us_layout[128] = {
+	0, 27, '1', '2', '3', '4', '5', '6', '7', '8',
+	'9', '0', '-', '=', '\b', '\t', 'q', 'w', 'e', 'r',
+	't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', 0,
+	'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
+	'\'', '`', 0, '\\', 'z', 'x', 'c', 'v', 'b', 'n',
+	'm', ',', '.', '/',  0, '*',  0, ' ', 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, '-', 0, 0, 0, '+', 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
+
+void kbd_isr()
+{
+	uint8_t scancode;
+
+	scancode = inb(PS2_REG_DATA);
+	// check if key release
+	if(scancode & 0x80)
+	{
+		//term_puti((uint32_t)scancode);
+		term_putchar(us_layout[scancode-128]);
+	}
+}
+
 void ps2_init()
 {
+	isr_register_isr(33, &kbd_isr);
+
 	// initialize usb controllers to prevent interference
 	// see if the ps/2 controller actually exists
 
