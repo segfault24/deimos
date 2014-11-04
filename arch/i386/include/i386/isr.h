19f7158dbe24ffe1e9fcdf5a42d7edf4d7a0f303
austin <austin@helios>
2014-11-04 03:21:21 -0500

    merged irqs with isrs. setup isr handler registration functions. still need to write out all wrapper stubs

--- arch/i386/include/i386/isr.h
+++ arch/i386/include/i386/isr.h
@@ -3,8 +3,7 @@
 
 #include <stdint.h>
 
-// assembly wrapper stubs that call our
-// generic interrupt handler
+// wrapper stubs that call our generic interrupt handler
 extern void isr0w();  // divide by zero
 extern void isr1w();  // debug
 extern void isr2w();  // non maskable interrupt
@@ -25,11 +24,36 @@ extern void isr18w(); // machine check
 extern void isr19w(); // SIMD floating point exception
 extern void isr20w(); // virtualization exception
 extern void isr30w(); // security exception
+// 32-47 are technically mapped IRQs
+extern void isr32w(); // programmable interrupt timer
+extern void isr33w(); // keyboard
+extern void isr35w(); // COM2
+extern void isr36w(); // COM1
+extern void isr37w(); // LPT2
+extern void isr38w(); // floppy disk
+extern void isr39w(); // LPT1/spurious
+extern void isr40w(); // CMOS real time clock
+extern void isr41w(); // (free)
+extern void isr42w(); // (free)
+extern void isr43w(); // (free)
+extern void isr44w(); // PS/2 mouse
+extern void isr45w(); // FPU/coprocessor/inter-processor
+extern void isr46w(); // primary ATA hard disk
+extern void isr47w(); // secondary ATA hard disk
 
-// actual interrupt handling goes here
-void isr_handler(uint8_t interrupt);
+// generic interrupt handler for use only by the asm wrappers
+void isr_handler(uint8_t interrupt, uint32_t error);
 
-// initializes and registers ISRs with the IDT
+// These are wrappers for functions in i386/idt.h that also perform
+// the necessary setup and clean up for the differences between
+// CPU exceptions (int 0-31) and hardware IRQs (int 32-47).
+//
+// Registered ISRs should be in the form "void fname(uint32_t error)".
+// These ISRs should be made as short as possible.
+// Error numbers have no meaning for IRQs, and can be discarded.
+void isr_register_isr(uint8_t interrupt, void (*func_ptr));
+void isr_clear_isr(uint8_t interrupt);
+
 void isr_init();
 
 #endif
--- arch/i386/isr_wrappers.s
+++ arch/i386/isr_wrappers.s
@@ -1,43 +1,48 @@
-.global isr0w
-.global isr1w
-.global isr2w
-.global isr3w
-.global isr4w
-.global isr5w
-.global isr6w
-.global isr7w
-.global isr8w
-.global isr10w
-.global isr11w
-.global isr12w
-.global isr13w
-.global isr14w
-.global isr16w
-.global isr17w
-.global isr18w
-.global isr19w
-.global isr20w
-.global isr30w
+.global isr0w, isr1w, isr2w, isr3w, isr4w
+.global isr5w, isr6w, isr7w, isr8w, isr10w
+.global isr11w, isr12w, isr13w, isr14w, isr16w
+.global isr17w, isr18w, isr19w, isr20w, isr30w
+.global isr32w, isr33w, isr34w, isr35w, isr36w
+.global isr37w, isr38w, isr39w, isr40w, isr41w
+.global isr42w, isr43w, isr44w, isr45w, isr46w
+.global isr47w
 
 .align 4
 .extern isr_handler
 
-# TODO: do we need to load the kernel data
-#       segment descriptor before calling
-#       the common handler?
+# TODO: do we need to load the kernel data (or possibly code)
+#       segment descriptor before calling the common handler?
 
+# shared interrupt return stub
+cmnret:
+	addl $8, %esp
+	popal
+	iret
+
+# cpu exceptions
 isr8w: # double fault
 	pushal
 	push $8
 	call isr_handler
-	addl $8, %esp
-	popal
-	iret
+	jmp cmnret
 
 isr13w: # general protection fault
 	pushal
 	pushl $13
 	call isr_handler
-	addl $8, %esp
-	popal
-	iret
+	jmp cmnret
+
+# hardware interrupt requests
+isr32w: # programmable interrupt timer
+	pushal
+	pushl $0 # dummy
+	pushl $32
+	call isr_handler
+	jmp cmnret
+
+isr33w: # keyboard interrupt
+	pushal
+	pushl $0 # dummy
+	pushl $33
+	call isr_handler
+	jmp cmnret;
