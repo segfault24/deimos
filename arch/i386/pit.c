af4f30650a97ed7377d3006ca59107ff5b18e4b6
austin <austin@helios>
2014-11-05 17:26:47 -0500

    timer isr moved to pit initialization

--- arch/i386/pit.c
+++ arch/i386/pit.c
@@ -1,7 +1,10 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <i386/isr.h>
 #include <i386/ioasm.h>
 #include <i386/pit.h>
+
+void tmr_isr(){}
 
 void pit_init()
 {
@@ -10,4 +13,6 @@ void pit_init()
 	// set the reload value
 	outb(PIT_CH0_DATA, PIT_CH0_RLDL);
 	outb(PIT_CH0_DATA, PIT_CH0_RLDH);
+
+	isr_register_isr(32, &tmr_isr);
 }
