804a49fe2c347c60c76f44539fa4fdb2c0d6c046
austin <austin@helios>
2014-11-07 02:59:32 -0500

    testing changes

--- arch/i386/arch.c
+++ arch/i386/arch.c
@@ -5,6 +5,7 @@
 #include <i386/pit.h>
 #include <i386/ps2.h>
 #include <i386/pmem_mgr.h>
+#include <i386/vmem_mgr.h>
 
 #include <kernel/tty.h>
 
@@ -17,39 +18,45 @@
 //   - interrupts are disabled
 // https://www.gnu.org/software/grub/manual/multiboot/multiboot.html#Machine-state
 
-void test_stat()
+void print_page_table(pd_entry pde, page_table* pt)
 {
-	tty_puts("    Free:");
-	tty_puti(pmem_free_blocks());
-	tty_puts(" Used:");
-	tty_puti(pmem_used_blocks());
-	tty_puts(" Total:");
-	tty_puti(pmem_total_blocks());
-	tty_puts("\n");
+	pt_entry pte;
+	size_t i;
+
+	for(i=0; i<PT_PAGES_PER_TABLE; i++)
+	{
+		pte = pt->entries[i];
+		if(pt_entry_is_present(pte))
+		{
+			tty_puts("PDE: ");
+			tty_puti((uint32_t)pde);
+			tty_puts("  PTE: ");
+			tty_puti((uint32_t)pte);
+			tty_puts("  \n");
+		}
+		tty_getchar();
+	}
 }
 
-void* test_alloc()
+void print_page_directory(page_directory* pd)
 {
-	void* ptr = pmem_mgr_alloc();
-	tty_puts("Alloc block: ");
-	tty_puti((uint32_t)ptr);
-	test_stat();
-	return ptr;
-}
+	pd_entry pde;
+	page_table* pt;
+	size_t i;
 
-void test_free(void* ptr)
-{
-	pmem_mgr_free(ptr);
-	tty_puts("Freed block: ");
-	tty_puti((uint32_t)ptr);
-	test_stat();
+	for(i=0; i<PD_TABLES_PER_DIR; i++)
+	{
+		pde = pd->entries[i];
+		if(pd_entry_is_present(pde))
+		{
+			pt = (page_table*)(pde & ~0xfff);
+			print_page_table(pde, pt);
+		}
+	}
 }
 
 void arch_init()
 {
-	//size_t i;
-	//void* ptrs[64];
-
 	// initializations MUST be done as in the sequence below
 	// otherwise we might end up trying to add ISRs to an
 	// improperly initialized IDT or cause bad things
@@ -60,13 +67,14 @@ void arch_init()
 	pit_init(); // setup the interrupt timer for a sysclock
 	ps2_init(); // setup the ps/2 controller(s)
 
-	// TODO: test the physical memory manager
-	//pmem_mgr_init(1024); // setup the low level memory manager
-	//pmem_mgr_reserve_region((void*)0, (void*)(16*4096-1));
-	//test_stat();
-	//for(i=0; i<1024; i++)
-	//	ptrs[i] = test_alloc();
-	//test_free(ptrs[0]);
+	// TODO: properly detect & reserve memory
+	pmem_mgr_init(PMEM_MAX_BLOCKS);
+	pmem_mgr_reserve_region(0x00000000, 0x00400000);
+
+	// TODO: test the virtual memory manager
+	vmem_mgr_init();
 
 	enable_interrupts(); // now we're ready
+
+	print_page_directory(vmem_mgr_get_directory());
 }
