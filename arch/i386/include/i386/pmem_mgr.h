70b97919478bc5de8ec082330d7ff25cebf04230
Austin Selick-Bottos <segfault24@users.noreply.github.com>
2014-11-07 17:54:07 -0500

    changed physical memory manager, should be more robust now

--- arch/i386/include/i386/pmem_mgr.h
+++ arch/i386/include/i386/pmem_mgr.h
@@ -13,15 +13,14 @@ typedef uint32_t phys_addr;
 typedef uint32_t virt_addr;
 
 void pmem_mgr_init();
+void pmem_mgr_free_region(phys_addr start, phys_addr end);
 
 uint32_t pmem_total_blocks();
 uint32_t pmem_free_blocks();
 uint32_t pmem_used_blocks();
 
-void pmem_mgr_reserve_region(phys_addr start, phys_addr end);
-void pmem_mgr_free_region(phys_addr start, phys_addr end);
-
 phys_addr pmem_mgr_alloc();
+// freeing addresses not allocated produces undefined behavior
 void pmem_mgr_free(phys_addr addr);
 
 #endif
