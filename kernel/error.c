da8f3bda888c40b9a917f874f63a026de42ad7f1
Austin Selick-Bottos <segfault24@github.com>
2015-02-25 20:25:27 -0500

    changed tty_puts to the new printf

--- kernel/error.c
+++ kernel/error.c
@@ -1,31 +1,27 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <kernel/stdio.h>
 #include <kernel/tty.h>
 #include <kernel/error.h>
 
 void kwarn(const char* message)
 {
 	tty_setcolor(VGA_YELLOW, VGA_BLACK);
-	tty_puts("\nWarning: ");
-	tty_puts(message);
-	tty_puts("\n");
+	puts("\nWarning: %s\n", message);
 }
 
 void kerror(const char* message)
 {
 	tty_setcolor(VGA_LIGHT_RED, VGA_BLACK);
-	tty_puts("\nError: ");
-	tty_puts(message);
-	tty_puts("\n");
+	puts("\nError: %s\n", message);
 }
 
 void kpanic(const char* message)
 {
 	tty_setcolor(VGA_RED, VGA_BLACK);
-	tty_puts("\nPanic: the kernel encountered an unrecoverable error\n");
-	tty_puts("Panic: >> ");
-	tty_puts(message);
-	tty_puts("\nPanic: system will now halt...");
+	printf("\nPanic: the kernel encountered an unrecoverable error\n");
+	printf("Panic: >> %s\n", message);
+	printf("\nPanic: system will now halt...");
 
 	for(;;);
 }
