5ba1f4549e2fa3ad63c378616c65e624cb7aaf78
Austin Selick-Bottos <segfault24@users.noreply.github.com>
2015-12-11 11:40:10 -0500

    moved tick counter to sched.c and soft coded kernel switching freq

--- include/i386/pit.h
+++ include/i386/pit.h
@@ -18,21 +18,10 @@
 #ifndef I386_PIT_H
 #define I386_PIT_H
 
-#define PIT_CH0_DATA 0x40
-#define PIT_CH2_DATA 0x42
-#define PIT_CMD 0x43
+#define BEEP_MIN_FREQ 100
+#define BEEP_MAX_FREQ 44100
 
-// set channel 0 to mode 3 (square wave)
-#define PIT_CH0_CFG 0x36
-// with a reload value of 0xFFFF
-// which is a rate of 18.2065Hz
-#define PIT_CH0_RLDH 0xFF
-#define PIT_CH0_RLDL 0xFF
-
-// set channel 2 to mode 3 (square wave)
-#define PIT_CH2_CFG 0xB6
-
-void beep();
+void beep(unsigned int freq, unsigned int millis);
 
 void pit_init();
 
